c8bfdffe53ee8655883484ab2e671e51b5865020
Joel16 <sauskethunderkid@gmail.com>
2015-03-21 12:30:39 -0500

    Update display module

--- prx/display/main.c
+++ prx/display/main.c
@@ -1,6 +1,9 @@
 #include <pspsdk.h>
 #include <pspkernel.h>
 #include <pspdisplay_kernel.h>
+#include <pspimpose_driver.h>
+#include <pspsysreg.h>
+#include <pspsysmem_kernel.h>
 
 PSP_MODULE_INFO("brightness", 0x1000, 1, 1);
 PSP_MAIN_THREAD_ATTR(0);
@@ -21,6 +24,24 @@ void setBrightness(int brightness){
     k1 = pspSdkSetK1(0);
     sceDisplaySetBrightness(brightness, 0);
     pspSdkSetK1(k1);
+}
+
+int imposeGetBrightness(){
+	u32 k1; 
+	
+    k1 = pspSdkSetK1(0);
+    int ret = sceImposeGetParam(PSP_IMPOSE_BACKLIGHT_BRIGHTNESS);
+    pspSdkSetK1(k1);
+    return ret;
+}
+
+int imposeSetBrightness(int value){
+	u32 k1; 
+
+    k1 = pspSdkSetK1(0);
+    int ret = sceImposeSetParam(PSP_IMPOSE_BACKLIGHT_BRIGHTNESS, value);
+    pspSdkSetK1(k1);
+    return ret;
 }
 
 int module_start(SceSize args, void *argp) 
