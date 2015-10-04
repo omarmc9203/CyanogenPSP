c33414919e225adc773c8acd7a6d1e126ae67ede
Joel16 <sauskethunderkid@gmail.com>
2015-10-03 22:07:14 -0500

    Delete more unnecessary files

--- prx/display/main.c
+++ prx/display/main.c
@@ -4,9 +4,15 @@
 #include <pspimpose_driver.h>
 #include <pspsysreg.h>
 #include <pspsysmem_kernel.h>
+#include <psppower.h>
 
 PSP_MODULE_INFO("brightness", 0x1000, 1, 1);
 PSP_MAIN_THREAD_ATTR(0);
+
+int sceDisplayEnable(void);
+int sceDisplayDisable(void);
+int sceDisplayEnable371(void);
+int sceDisplayDisable371(void);
 
 int getBrightness(){
     int currentBrightness = 0;
@@ -40,6 +46,28 @@ int imposeSetBrightness(int value){
 
     k1 = pspSdkSetK1(0);
     int ret = sceImposeSetParam(PSP_IMPOSE_BACKLIGHT_BRIGHTNESS, value);
+    pspSdkSetK1(k1);
+    return ret;
+}
+
+int displayEnable(void){
+    u32 k1 = pspSdkSetK1(0);
+    int ret;
+    if (sceKernelDevkitVersion() < 0x03070110)
+        ret = sceDisplayEnable();
+    else
+        ret = sceDisplayEnable371();
+    pspSdkSetK1(k1);
+    return ret;
+}
+
+int displayDisable(void){
+    u32 k1 = pspSdkSetK1(0);
+	int ret;
+    if (sceKernelDevkitVersion() < 0x03070110)
+        ret = sceDisplayDisable();
+    else
+        ret = sceDisplayDisable371();
     pspSdkSetK1(k1);
     return ret;
 }
