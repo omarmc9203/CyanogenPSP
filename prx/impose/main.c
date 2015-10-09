0bad7d3bf29d4182c33e4616b733fe7360b0110a
Joel16 <sauskethunderkid@gmail.com>
2015-10-09 17:36:22 -0500

    bring up kernel imported impose.prx

--- prx/impose/main.c
+++ prx/impose/main.c
@@ -1,27 +1,9 @@
-//    LightMP3
-//    Copyright (C) 2007 Sakya
-//    sakya_tg@yahoo.it
-//
-//    This program is free software; you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation; either version 2 of the License, or
-//    (at your option) any later version.
-//
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with this program; if not, write to the Free Software
-//    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include <pspsdk.h>
 #include <pspkernel.h>
 #include <pspimpose_driver.h>
-#include <pspsysreg.h>
 #include <pspsysmem_kernel.h>
 
-PSP_MODULE_INFO("soundlib", 0x1006, 1, 1);
+PSP_MODULE_INFO("imposeLib", 0x1006, 1, 1);
 PSP_MAIN_THREAD_ATTR(0);
 
 u32 k1;
@@ -64,6 +46,13 @@ int imposeGetEqualizer(){
 int imposeSetEqualizer(int value){
     k1 = pspSdkSetK1(0);
     int ret = sceImposeSetParam(PSP_IMPOSE_EQUALIZER_MODE, value);
+    pspSdkSetK1(k1);
+    return ret;
+}
+
+int imposeSetHomePopup(int value){
+    k1 = pspSdkSetK1(0);
+    int ret = sceImposeSetHomePopup(value);
     pspSdkSetK1(k1);
     return ret;
 }
